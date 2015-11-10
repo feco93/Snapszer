ade1ac3dc9f16caaa020e42573881850bbe7238f
Ferenc Sipos <siposferenc93@gmail.com>
2015-11-10 23:09:21 +0100

    Modify table traslation and scale.

--- src/main/java/hu/unideb/snapszer/MainApp.java
+++ src/main/java/hu/unideb/snapszer/MainApp.java
@@ -22,69 +22,70 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 
 public class MainApp extends Application {
-    
-    private static final double MODEL_SCALE_FACTOR = 20;
-    
+
+    private static final double MODEL_SCALE_FACTOR = 5;
+
     private Scene scene;
     private Group root;
     private PointLight pointLight;
     double anchorX, anchorY, anchorAngle;
-    
+
     private void buildScene() {
-        
+
         TdsModelImporter importer = new TdsModelImporter();
         importer.setResourceBaseUrl(getClass().getResource("/3ds"));
         importer.read(getClass().getResource("/3ds/POKER+TABLE.3ds"));
         Node[] nodes = importer.getImport();
         Node table = nodes[0];
-        table.setScaleX(MODEL_SCALE_FACTOR);
-        table.setScaleY(MODEL_SCALE_FACTOR);
-        table.setScaleZ(MODEL_SCALE_FACTOR);
-                root.getChildren().addAll(table);
+        double x = (table.getBoundsInLocal().getMaxX() + table.getBoundsInLocal().getMinX()) / 2;
+        double y = (table.getBoundsInLocal().getMaxY() + table.getBoundsInLocal().getMinY()) / 2;
+        double z = (table.getBoundsInLocal().getMaxZ() + table.getBoundsInLocal().getMinZ()) / 2;
+        table.getTransforms().addAll(
+                new Scale(MODEL_SCALE_FACTOR, MODEL_SCALE_FACTOR, MODEL_SCALE_FACTOR),
+                new Translate(-x, -y, -z)
+        );
         
+        y = table.getBoundsInParent().getMaxY();
+        table.setTranslateY(y);
+        root.getChildren().addAll(table);
+
         pointLight = new PointLight(Color.WHITE);
-        pointLight.setTranslateZ(-350);
-        pointLight.setTranslateY(-1000);
-        pointLight.getScope().add(table);
-        root.getChildren().add(pointLight);        
+        pointLight.setTranslateY(-200);
+        pointLight.getScope().addAll(table);
+        root.getChildren().add(pointLight);
     }
-    
+
     private PerspectiveCamera addCamera(Scene scene) {
-        PerspectiveCamera perspectiveCamera = new PerspectiveCamera();
+        PerspectiveCamera perspectiveCamera = new PerspectiveCamera(true);
         double NEAR = 0.01;
         double FAR = 1000;
         perspectiveCamera.setFarClip(FAR);
         perspectiveCamera.setNearClip(NEAR);
-        perspectiveCamera.setFieldOfView(60);
+        perspectiveCamera.setTranslateY(-350);
+        perspectiveCamera.setTranslateZ(-330);
         perspectiveCamera.setRotationAxis(Rotate.X_AXIS);
-        perspectiveCamera.setRotate(-55);
-        scene.widthProperty().addListener((obj, oldValue, newValue) -> {
-            perspectiveCamera.setTranslateX(-1 * (newValue.doubleValue() / 2));
-        });
-        scene.heightProperty().addListener((obj, oldValue, newValue) -> {
-            perspectiveCamera.setTranslateY(-1 * (newValue.doubleValue() / 2) + 550);
-        });
-        perspectiveCamera.setTranslateZ(400);
+        perspectiveCamera.setRotate(-50);
         scene.setCamera(perspectiveCamera);
         return perspectiveCamera;
     }
-    
+
     @Override
     public void start(Stage primaryStage) {
         root = new Group();
-        
+
         HungarianCardView cardView
                 = new HungarianCardView(new HungarianCard(HungarianCardRank.ALSO, HungarianCardSuit.TOK));
         cardView.getTransforms().addAll(
-                new Rotate(-50, Rotate.X_AXIS),
+                new Rotate(-90, Rotate.X_AXIS),
                 new Rotate(180, Rotate.Y_AXIS),
-                new Rotate(0, Rotate.Z_AXIS)
+                new Rotate(0, Rotate.Z_AXIS),
+                new Scale(0.3, 0.3, 0.3)
         );
-        cardView.setTranslateY(300);
-        cardView.setTranslateZ(-250);
+        cardView.setTranslateY(-3);
         cardView.setOnMouseEntered((MouseEvent event) -> {
             scene.setCursor(Cursor.HAND);
         });
@@ -100,7 +101,7 @@ public class MainApp extends Application {
                 anchorAngle = cardView.getRotate();
             }
         });
-        
+
         cardView.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -122,11 +123,11 @@ public class MainApp extends Application {
                 );
             }
         });
-        
+
         AmbientLight light = new AmbientLight(Color.WHITE);
         light.getScope().add(cardView);
         root.getChildren().add(light);
-        
+
         buildScene();
         root.getChildren().add(cardView);
         scene = new Scene(root, 0, 0, true, SceneAntialiasing.BALANCED);
@@ -145,9 +146,9 @@ public class MainApp extends Application {
         addCamera(scene);
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
